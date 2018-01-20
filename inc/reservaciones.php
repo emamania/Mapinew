38b4c6203b52b2d4200739eb99f0fce00e296a86
Elvis <centinela1526@gmail.com>
2018-01-20 00:13:52 -0500

    Insertar datos al Formulario

--- inc/reservaciones.php
+++ inc/reservaciones.php
@@ -8,16 +8,20 @@ function ema_guardar() {
 		$email 			= sanitize_text_field( $_POST['email_booking'] );
 		$phone 			= sanitize_text_field( $_POST['phone_booking'] );
 		$whatsaap 	= sanitize_text_field( $_POST['whatsapp_booking'] );
+		$check_in 	= sanitize_text_field( $_POST['fecha_in_booking'] );
+		$check_out 	= sanitize_text_field( $_POST['fecha_out_booking'] );
 		$adulto 		= sanitize_text_field( $_POST['adulto_booking'] );
 		$child 			= sanitize_text_field( $_POST['nino_booking'] );
 		$mensaje 		= sanitize_text_field( $_POST['mensaje_booking'] );
 	endif;
 
 	$datos = array(
-		'Nombre' 		=> $nombre,
+		'nombre' 		=> $nombre,
 		'email'			=> $email,
 		'phone'			=> $phone,
 		'whatsapp'	=> $whatsaap,
+		'check_in'	=> $check_in,
+		'check_out'	=> $check_out,
 		'adulto'		=> $adulto,
 		'child'			=> $child,
 		'mensaje'		=> $mensaje

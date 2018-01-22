2c67ec51480c7192afad570443ddb8feca6dba86
Elvis <centinela1526@gmail.com>
2018-01-22 01:12:10 -0500

    Agregando un requerimiento opciones

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -16,7 +16,7 @@ function ema_load_scripts(){
   wp_deregister_script( 'jquery' );
   wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery.js', false, '1.12.4', true );
 	wp_enqueue_script( 'jquery' );
-	
+
   wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '3.3.7', true );
 	wp_enqueue_script( 'script', get_template_directory_uri() . '/js/main.js', array( 'jquery' ), '1.0', true );
 }
